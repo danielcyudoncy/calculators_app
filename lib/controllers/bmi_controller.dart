dc01b51808f10ebcbf20b695dce0e8c6527d5f70
Daniel Udoncy <113753369+danielcyudoncy@users.noreply.github.com>
2024-07-18 09:08:40 +0100

    adding controllers

---
+++ lib/controllers/bmi_controller.dart
@@ -0,0 +1,14 @@
+// controllers/bmi_controller.dart
+import 'package:get/get.dart';
+
+class BMIController extends GetxController {
+  var bmiModel = BMIModel(height: 0.0, weight: 0.0).obs;
+
+  void calculateBMI() {
+    double heightInMeters = bmiModel.value.height / 100;
+    double bmi = bmiModel.value.weight / (heightInMeters * heightInMeters);
+    bmiModel.update((model) {
+      model?.bmi = bmi;
+    });
+  }
+}
