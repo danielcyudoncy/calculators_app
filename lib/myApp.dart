7903a49e2496eceb50605b972ab14b20fea27f74
Daniel Udoncy <113753369+danielcyudoncy@users.noreply.github.com>
2024-07-18 08:59:07 +0100

    creating main, app and homepages

---
+++ lib/myApp.dart
@@ -0,0 +1,17 @@
+import 'package:calculators_app/homepage.dart';
+import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'calculators',
+      theme: ThemeData(),
+      home: const Homepage(),
+    );
+  }
+}
